717d9e9953b66917b9ef8287a7fdb9d878b8e875
nictru <nictru32@gmail.com>
2025-08-15 07:32:33 +0200

    Set up custom element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  encapsulation: ViewEncapsulation.ShadowDom,
 })
 export class AppComponent {
-  title = 'visage';
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { createApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
+import { ApplicationRef } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+(async () => {
+  const app: ApplicationRef = await createApplication(appConfig);
+
+  // Define Web Components
+  const visageComponent = createCustomElement(AppComponent, {
+    injector: app.injector,
+  });
+  customElements.define('visa-ge', visageComponent);
+})();
