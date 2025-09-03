3e327e78b269941b3068da47af6d41ee0b48cadb
nictru <nictru32@gmail.com>
2025-09-03 16:54:08 +0200

    Implement first outside interfacing

--- src/app/visage.component.ts
+++ src/app/visage.component.ts
@@ -1,4 +1,13 @@
-import { Component, computed, inject, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  computed,
+  effect,
+  inject,
+  ViewEncapsulation,
+} from '@angular/core';
 import { State } from './state.store';
 import { GenomeSelectorComponent } from './genome-selector/genome-selector.component';
 import { AppComponent } from './app/app.component';
@@ -11,6 +20,20 @@ import { AppComponent } from './app/app.component';
   encapsulation: ViewEncapsulation.ShadowDom,
 })
 export class VisageComponent {
+  @Output() genomeIdChange = new EventEmitter<string | null>();
+
+  @Input({ alias: 'genome-id' })
+  set genomeId(value: string | null) {
+    this.state.setGenomeId(value ?? null);
+  }
+
   state = inject(State);
   hasGenome = computed(() => this.state.genomeId() !== null);
+
+  constructor() {
+    effect(() => {
+      const currentGenomeId = this.state.genomeId();
+      this.genomeIdChange.emit(currentGenomeId);
+    });
+  }
 }
